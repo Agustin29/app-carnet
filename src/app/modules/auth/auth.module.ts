44756727dda97b2639b9891216c783b2c34f1d3a
agtbta29 <agustinbatista29@gmail.com>
2023-12-04 15:29:26 -0500

    completado diseño de login

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
+/* PRIMENG */
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent,
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    CardModule,
+    ButtonModule,
+    DividerModule,
+    InputTextModule
   ]
 })
 export class AuthModule { }
